8332848b11351b6b14e086c0a7557d64af18ede2
Setrotec <67295288+Setrotec@users.noreply.github.com>
2021-04-20 17:21:26 -0300

    Alterando registros

---
+++ Alterando_Registros.sql
@@ -0,0 +1,12 @@
+
+/*Alterando o registro com UPDATE*/
+UPDATE [TABELA DE PRODUTOS] SET 
+/*Inserindo 'lata' no campo [EMBALAGEM]*/
+[EMBALAGEM] = 'Lata',
+[PREÇO DE LISTA] = 2.46
+
+/* Condição de busca pelo campo [CODIGO DO PRODUTO] */
+WHERE [CODIGO DO PRODUTO] = '544931'
+
+
+
