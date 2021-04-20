338d59d66be0ca81e593d1c9649bc9e6f56c8190
Setrotec <67295288+Setrotec@users.noreply.github.com>
2021-04-20 17:22:39 -0300

    Alterando campo para valores NOT NULL

---
+++ Alterando_campo_not_NULL.sql
@@ -0,0 +1,6 @@
+/*Alterando estrutura da tabela */
+ALTER TABLE [TABELA DE PRODUTOS]
+/*Alterando o campo da tabela */
+ALTER COLUMN [CODIGO DO PRODUTO]
+/*Alterando o campo para restringir vlores NULL */
+VARCHAR(10) NOT NULL
