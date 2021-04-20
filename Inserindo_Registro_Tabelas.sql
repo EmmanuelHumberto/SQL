9a9c3ac3df10fbbf987e8c2a43cc19d6694da22f
Setrotec <67295288+Setrotec@users.noreply.github.com>
2021-04-20 17:32:11 -0300

    Inserindo registro

---
+++ Inserindo_Registro_Tabelas.sql
@@ -0,0 +1,8 @@
+/*Inserindo dados na tabela*/
+INSERT INTO [TABELA DE PRODUTOS]
+/*Campos da tabela passados como parametro para inserção de dados*/
+([CODIGO DO PRODUTO], [NOME DO PRODUTO], [EMBALAGEM], [TAMANHO], [SABOR], [PREÇO DE LISTA])
+VALUES
+/*Dados a serem inseridos mo seus respectivos campos*/
+('1040107','Light - 350 ml - Melância', 'Lata', '350 ml', 'Melância', 4.56)
+
