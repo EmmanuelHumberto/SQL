4b2a2003de6c35b690aa39530402ba05997c0dea
Setrotec <67295288+Setrotec@users.noreply.github.com>
2021-04-20 17:28:25 -0300

    Excuindo banco

---
+++ Apagando_Banco_de_Dados.sql
@@ -0,0 +1,9 @@
+/*Apagando banco de dados*/
+DROP DATABASE [BANCO_EX]
+
+/*Apagando tabela*/
+DROP TABLE [TABELA_EX]
+
+/*Deleta banco, tabela, campo ou valor*/
+DELETE [BANCO_EX] WHERE [TABELA_EX] = valor
+
